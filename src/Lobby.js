a7de23148b4c3981e559141edad1299424e81496
Lee <leewhiz119@gmail.com>
2023-07-24 14:33:11 -0400

    created lobby_header and lobby_content container divs

--- src/Lobby.js
+++ src/Lobby.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Lobby() {
   return (
-    <div className="lobby">Lobby</div>
+    <div className="lobby">
+        <div className="lobby_header">
+        </div>
+        <div className="lobby_content"></div>
+
+    </div>
   )
 }
