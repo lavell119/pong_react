59f6b89ea4dfa45ed26faccde690d3b414727888
Lee <leewhiz119@gmail.com>
2023-07-24 14:44:12 -0400

    created lobby_menu, lobby_menu_items, and styled them and page colors

--- src/Lobby.js
+++ src/Lobby.js
@@ -4,6 +4,13 @@ export default function Lobby() {
   return (
     <div className="lobby">
         <div className="lobby_header">
+            <div className="lobby_menu">
+                <div className="lobby_menu_item"></div>
+                <div className="lobby_menu_item"></div>
+                <div className="lobby_menu_item"></div>
+                <div className="lobby_menu_item"></div>
+
+            </div>
         </div>
         <div className="lobby_content"></div>
 
