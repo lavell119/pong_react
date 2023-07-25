5292986f5d26287d210b9ca800d6c1edf0193bd3
Lee <leewhiz119@gmail.com>
2023-07-24 23:32:57 -0400

    imported react-router-dom and created routes for lobby and table and created Table.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Lobby from './Lobby'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Table from './Table'
 
 function App() {
   return (
     <div className="App">
-      <Lobby />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path ="/" element = {<Lobby />}></Route>
+          <Route exact path ="/table/:id" element = {<Table />}></Route>
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
