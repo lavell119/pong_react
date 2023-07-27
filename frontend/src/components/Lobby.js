781361ef548fba9fdf355b254a069c1cee4f00f8
Lee <leewhiz119@gmail.com>
2023-07-27 13:07:04 -0400

    succesfully imported envelope icon from font awesome and rendered it inside lobby-menu

--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import LobbyTables from './LobbyTables'
 
 export default function Lobby() {
@@ -6,7 +8,7 @@ export default function Lobby() {
     <div className="lobby">
         <div className="lobby_header">
             <div className="lobby_menu">
-                <div className="lobby_menu_item"><FontAwesomeIcon icon={faGamepad} size="sm" style={{color: "#ffffff",}} /></div>
+                <div className="lobby_menu_item"><FontAwesomeIcon icon={faEnvelope} size="sm" style={{color: "#ffffff",}} /></div>
                 <div className="lobby_menu_item">Cashier</div>
                 <div className="lobby_menu_item">Settings</div>
                 <div className="lobby_menu_item">Profile</div>
