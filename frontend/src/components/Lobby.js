4c1b4b3b2f90a46343d9e3c96d16bc1656932922
Lee <leewhiz119@gmail.com>
2023-07-27 13:48:07 -0400

    added lobby and cashier logos (xl size) to lobby-menu

--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
+import { faWallet } from '@fortawesome/free-solid-svg-icons'
 import LobbyTables from './LobbyTables'
 
 export default function Lobby() {
@@ -8,8 +10,8 @@ export default function Lobby() {
     <div className="lobby">
         <div className="lobby_header">
             <div className="lobby_menu">
-                <div className="lobby_menu_item"><FontAwesomeIcon icon={faEnvelope} size="sm" style={{color: "#ffffff",}} /></div>
-                <div className="lobby_menu_item">Cashier</div>
+                <div className="lobby_menu_item"><FontAwesomeIcon icon={faGamepad} size="xl" style={{color: "#ffffff",}} />Lobby</div>
+                <div className="lobby_menu_item"><FontAwesomeIcon icon={faWallet} size="xl" style={{color: "#ffffff",}} />Cashier</div>
                 <div className="lobby_menu_item">Settings</div>
                 <div className="lobby_menu_item">Profile</div>
 
