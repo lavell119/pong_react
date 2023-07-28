201843987af5a634f3fcfd0bd806e7c621c9bc95
Lee <leewhiz119@gmail.com>
2023-07-27 22:14:21 -0400

    changed input fields to divs and added label spans inside for field content inside profile-popup

--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -22,30 +22,55 @@ export default function Lobby() {
         </div>
         <div className="profile_popup">
           <div className="profile_popup_content">
-            <h2>My Profile</h2>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input><input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
+            <h3>MY PROFILE</h3>
+            <div className='profile_popup_field'>
+              <span>Username</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>E-Mail Address</span>
+              </div>
+            <div className='profile_popup_field'>
+              <span>First Name</span>
+            </div>
+            <div className='profile_popup_field'>
+            <span>Last Name</span>
+
+            </div>
+            <div className="profile_popup_field_divider">
+              <div className='mini_profile_popup_field'>
+                <span>Birthdate</span>
+              </div>
+              <div className='mini_profile_popup_field'>
+                <span>Gender</span>
+              </div>
+            </div>
+            
+            <div className='profile_popup_field'>
+              <span>Address Line 1</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>Address Line 2</span>
+
+            </div>
+            <div className='profile_popup_field'>
+              <span>City</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>State/ Province</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>Zip/ Postal Code</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>Mobile/ Cell Phone</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>Daytime Phone</span>
+            </div>
+            <div className='profile_popup_field'>
+              <span>Evening Phone</span>
+            </div>
+
           </div>                 
         </div>
         <div className="lobby_content">
