ffe73a0be973e0f6a33f02ac4c78108b2609b495
Lee <leewhiz119@gmail.com>
2023-08-01 15:14:29 -0400

    create cashierWidget component, imported inside CashierPopup and started working on adding content and styling

---
+++ frontend/src/components/cashierWidgets/Deposit.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Deposit() {
+  return (
+    <div className="cashier_widget deposit-widget">
+        <h3>DEPOSIT TO ACCOUNT</h3>
+        <div className="deposit_widget_content">
+            <div className="deposit_welcome">
+                <div className="deposit_welcome_top">
+                    <div>
+                        <div>Welcome User.</div>
+                        <div>Deposit Now!</div>
+                    </div>   
+                    <button>Live Chat</button>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+       
+    </div>
+  )
+}
--- frontend/src/components/popups/CashierPopup.js
+++ frontend/src/components/popups/CashierPopup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from 'react-redux'
+import Deposit from '../cashierWidgets/Deposit'
 
 export default function CashierPopup() {
     const dispatch = useDispatch()
@@ -21,7 +22,7 @@ export default function CashierPopup() {
         </div>
         <div className='cashier_popup_content'>
           <div className="cashier_popup_component">
-            jlb
+            <Deposit />
           </div>
         </div>
     </div>
