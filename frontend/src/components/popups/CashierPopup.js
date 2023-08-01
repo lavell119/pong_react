24989258dec3c72890517013191cb23eb7f82870
Lee <leewhiz119@gmail.com>
2023-08-01 00:12:55 -0400

    styled CashierPopup side, added CashierPopup header and added 'CASHIER' title and spaced it using two more divs for buttons and flexbox

--- frontend/src/components/popups/CashierPopup.js
+++ frontend/src/components/popups/CashierPopup.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function CashierPopup() {
   return (
-    <div className ="cashier_popup">CashierPopup</div>
+    <div className ="cashier_popup">
+        <div className="cashier_popup_header">
+            <div></div>
+            CASHIER
+            <div></div>
+        </div>
+    </div>
   )
 }
