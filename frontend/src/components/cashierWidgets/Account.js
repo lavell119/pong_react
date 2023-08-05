7f0d69a169081f8a925464f973159acc00a933dd
Lee <leewhiz119@gmail.com>
2023-08-05 18:53:18 -0400

    added height and overflow auto styles to cashier_widget to give Deposit.js content scroll functionality again

--- frontend/src/components/cashierWidgets/Account.js
+++ frontend/src/components/cashierWidgets/Account.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Account() {
   return (
-    <div className="cashier_widget account_widget">AccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccountAccount</div>
+    <div className="cashier_widget account_widget">
+        <p>Enter the date range below and then select 'Search' to see your past transactions.</p>
+    </div>
   )
 }
