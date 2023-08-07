d6d786a2ddb812a89ec696380f7404832b545f69
Lee <leewhiz119@gmail.com>
2023-08-07 16:09:56 -0400

    added 'stakes' prop to LobbyTable and rendered it inside LobbyTable stakes element

--- frontend/src/components/LobbyTable.js
+++ frontend/src/components/LobbyTable.js
@@ -3,13 +3,14 @@ import {useParams, Link} from 'react-router-dom'
 
 export default function LobbyTable(props) {
   const id =props.id
+  const stakes = props.stakes
   // const { id } = useParams()
   console.log('table: ',id)
   return (
     
           <div className="lobby_table">
             <h3> {`Table ${id}`} </h3> 
-            <text><b>Stakes:</b> 1/2 </text>
+            <text><b>Stakes:</b> {stakes} </text>
             <div className="lobby_table_table"></div>
             <Link to={`table/${id}`}>
               <button class="join_table_btn">Join Table</button>
--- frontend/src/components/LobbyTables.js
+++ frontend/src/components/LobbyTables.js
@@ -5,11 +5,11 @@ import LobbyTable from './LobbyTable'
 export default function LobbyTables() {
   return (
     <div className="lobby_tables">
-    <LobbyTable id="1" />
-    <LobbyTable id="2" />
-    <LobbyTable id="3" />
-    <LobbyTable id="4"/>
-    <LobbyTable id="5"/>
+    <LobbyTable id="1" stakes = "1/2"/>
+    <LobbyTable id="2" stakes = "1/2"/>
+    <LobbyTable id="3" stakes = "2/4"/>
+    <LobbyTable id="4 "stakes = "2/4"/>
+    <LobbyTable id="5" stakes = "4/8"/>
     </div>
     
 
