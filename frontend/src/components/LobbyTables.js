352ea1a9606880b81da8b89ab8204aaf63b8e772
Lee <leewhiz119@gmail.com>
2023-08-08 17:48:59 -0400

    installed slick slider and got it working inside Lobby

--- frontend/src/components/LobbyTables.js
+++ frontend/src/components/LobbyTables.js
@@ -12,18 +12,44 @@ export default function LobbyTables() {
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToScroll: 1,
     arrows: true
   }
 
   return (
-    <div className="lobby_tables">
+    <>
+    {/* <div className="lobby_tables">
     <LobbyTable id="1" stakes = "1/2"/>
     <LobbyTable id="2" stakes = "1/2"/>
     <LobbyTable id="3" stakes = "2/4"/>
     <LobbyTable id="4 "stakes = "2/4"/>
     <LobbyTable id="5" stakes = "4/8"/>
+    </div> */}
+    <div className="slider">
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
     </div>
+    </>
+    
+
   
    
 
--- frontend/src/components/cashierWidgets/Account.js
+++ frontend/src/components/cashierWidgets/Account.js
@@ -5,6 +5,7 @@ export default function Account() {
     <div className="cashier_widget account_widget">
         <p>Enter the date range below and then select 'Search' to see your past transactions.</p>
         <div>
+        
           <label for="from_date">From</label>
           <input id ="from_date"></input>
           <label for="to_date">To</label>
