b9aee96613d4b7d344ce55aa1326521dc8b4d706
Lee <leewhiz119@gmail.com>
2023-08-09 01:46:17 -0400

    added cursor pointer and hover border thicker to deposit option img

--- frontend/src/components/LobbyTables.js
+++ frontend/src/components/LobbyTables.js
@@ -28,16 +28,16 @@ export default function LobbyTables() {
     <div className="lobby_tables">
     <div className="slider">
         <Slider {...settings}>
-        <LobbyTable id="1" stakes = "1/2"/>
-    <LobbyTable id="2" stakes = "1/2"/>
-    <LobbyTable id="3" stakes = "2/4"/>
-    <LobbyTable id="4 "stakes = "2/4"/>
-    <LobbyTable id="5" stakes = "4/8"/>
     <LobbyTable id="1" stakes = "1/2"/>
     <LobbyTable id="2" stakes = "1/2"/>
     <LobbyTable id="3" stakes = "2/4"/>
     <LobbyTable id="4 "stakes = "2/4"/>
     <LobbyTable id="5" stakes = "4/8"/>
+    <LobbyTable id="6" stakes = "4/8"/>
+    <LobbyTable id="7" stakes = "5/10"/>
+    <LobbyTable id="8" stakes = "5/10"/>
+    <LobbyTable id="9 "stakes = "10/20"/>
+    <LobbyTable id="10" stakes = "10/20"/>
         </Slider>
     </div>
     </div>
